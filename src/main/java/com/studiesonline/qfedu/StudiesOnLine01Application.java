2182d09b041070e9d8638537de4b9ea553bbfcb5
Story <1614888370@qq.com>
2020-01-12 12:38:31 +0800

    学成在线

---
+++ src/main/java/com/studiesonline/qfedu/StudiesOnLine01Application.java
@@ -0,0 +1,13 @@
+package com.studiesonline.qfedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesOnLine01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudiesOnLine01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/studiesonline/qfedu/StudiesOnLine01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studiesonline.qfedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiesOnLine01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
